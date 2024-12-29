c7d8c731f7679ecbdca22a1cef42243ed3ffd525
chetex <chetex69@gmail.com>
2024-12-29 17:41:30 +0100

    New launch.json deno tests and deno configuration

--- Builder/tests/builder_test.ts
+++ Builder/tests/builder_test.ts
@@ -1,11 +1,13 @@
 import { QueryBuilder } from "../builders/QueryBuilder.ts";
 import { Director } from "../builders/Director.ts";
-import { Query } from "../models/Query.ts";
 import {
     assertEquals,
     assertStringIncludes,
 } from "https://deno.land/std@0.200.0/testing/asserts.ts";
 
+/**
+ * Build directly the QueryBuilder class without director.
+ */
 Deno.test("QueryBuilder: Basic select statement", () => {
     const builder = new QueryBuilder();
     const query = builder.select("id", "name").from("users").build();
@@ -13,6 +15,10 @@ Deno.test("QueryBuilder: Basic select statement", () => {
     assertEquals(query.toString(), "SELECT id, name FROM users");
 });
 
+/**
+ * Build directly the QueryBuilder class without director.
+ * Complex query with order and limit.
+ */
 Deno.test("QueryBuilder: Select with where clause", () => {
     const builder = new QueryBuilder();
     const query = builder
@@ -24,6 +30,10 @@ Deno.test("QueryBuilder: Select with where clause", () => {
     assertEquals(query.toString(), "SELECT id, name FROM users WHERE age > 18");
 });
 
+/**
+ * Build directly the QueryBuilder class without director.
+ * Complex query with two where clauses, order and limit.
+ */
 Deno.test("QueryBuilder: Complex query with order and limit", () => {
     const builder = new QueryBuilder();
     const query = builder
@@ -40,6 +50,10 @@ Deno.test("QueryBuilder: Complex query with order and limit", () => {
     assertEquals(query.toString(), expectedQuery);
 });
 
+/**
+ * Build directly the QueryBuilder class with director.
+ * Simple query
+ */
 Deno.test("Director: Constructs basic query", () => {
     const builder = new QueryBuilder();
     const director = new Director(builder);
@@ -48,6 +62,10 @@ Deno.test("Director: Constructs basic query", () => {
     assertEquals(query.toString(), "SELECT id, name, email FROM users");
 });
 
+/**
+ * Build directly the QueryBuilder class with director.
+ * Complex query
+ */
 Deno.test("Director: Constructs advanced query", () => {
     const builder = new QueryBuilder();
     const director = new Director(builder);
@@ -55,7 +73,10 @@ Deno.test("Director: Constructs advanced query", () => {
 
     assertStringIncludes(query.toString(), "SELECT id, name, email");
     assertStringIncludes(query.toString(), "FROM users");
-    assertStringIncludes(query.toString(), "WHERE age > 18 AND country = 'CRI'");
+    assertStringIncludes(
+        query.toString(),
+        "WHERE age > 18 AND country = 'CRI'",
+    );
     assertStringIncludes(query.toString(), "ORDER BY name ASC");
     assertStringIncludes(query.toString(), "LIMIT 10");
 });
