84bc10bd740a79e209edb3e3081b6cc8b17c8be7
chetex <chetex69@gmail.com>
2024-12-31 09:31:41 +0100

    Created new enum typ with ENUM and Interface restaurant. Removed switch case

--- FactoryPattern/main.ts
+++ FactoryPattern/main.ts
@@ -1,28 +1,30 @@
 import { RestaurantFactory } from "./Factory/RestaurantFactory.ts";
 import { RestaurantHardFactory } from "./Factory/RestaurantHardFactory.ts";
 import { RestaurantVeggiFactory } from "./Factory/RestaurantVeggiFactory.ts";
+import { BurgerTypeEnum } from "./enum/BurgerTypeEnum.ts";
+
+// Create Global map with RestaurantFactory and BurgerTypeEnum
+const restaurantMap = new Map<BurgerTypeEnum, RestaurantFactory>();
+
+function init() {
+    restaurantMap.set(BurgerTypeEnum.Hard, new RestaurantHardFactory());
+    restaurantMap.set(BurgerTypeEnum.Veggi, new RestaurantVeggiFactory());
+}
 
 function main() {
-    let restaurant: RestaurantFactory;
-
     const burgerType = prompt(
         "¿Qué tipo de hamburguesa quieres? ( Hard/Veggi )",
     );
 
-    switch (burgerType) {
-        case "Hard":
-            restaurant = new RestaurantHardFactory();
-            break;
-
-        case "Veggi":
-            restaurant = new RestaurantVeggiFactory();
-            break;
-
-        default:
-            throw new Error("Opción no válida");
+    // Verificar si enumKey y restaurant son válidos
+    const restaurant = restaurantMap.get(
+        BurgerTypeEnum[burgerType as keyof typeof BurgerTypeEnum],
+    );
+    if (!restaurant) {
+        throw new Error("Opción no válida");
     }
-
     restaurant.orderHamburger();
 }
 
+init();
 main();
